da4429fe0cb8bfafbfa3498490758f8f75ca2a60
fredrika-g <fredrika.gerring@gmail.com>
2024-02-20 14:21:46 +0100

    adding logout function, enabling toggling of actions based on logged in status

---
+++ logout.js
@@ -0,0 +1,15 @@
+logOutBtn.addEventListener("click", () => {
+  statusMsg.innerText = "";
+  users = JSON.parse(localStorage.getItem("users"));
+
+  users.forEach((user) => {
+    user.loggedIn = false;
+  });
+
+  let newUserList = [...users];
+
+  //   updating local storage
+  localStorage.setItem("users", JSON.stringify(newUserList));
+  localStorage.removeItem("loggedInUser");
+  toggleUserActions(1500, "Bye for now!");
+});
